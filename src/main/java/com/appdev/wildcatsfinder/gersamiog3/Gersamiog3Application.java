52d1cb33d8bedfc7635b6871bdcc8b75508ed171
unknown <rudyarddecember@gmail.com>
2025-10-21 22:24:13 +0800

    Initial commit - Spring Boot CRUD implementation

---
+++ src/main/java/com/appdev/wildcatsfinder/gersamiog3/Gersamiog3Application.java
@@ -0,0 +1,13 @@
+package com.appdev.wildcatsfinder.gersamiog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gersamiog3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gersamiog3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/appdev/wildcatsfinder/gersamiog3/Gersamiog3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.wildcatsfinder.gersamiog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gersamiog3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
